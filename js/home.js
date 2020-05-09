d28ad5d50ea4ce1f8955a470d4eb784e9db41732
unknown <arfeenmalik33@gmail.com>
2020-05-09 03:07:33 -0700

    First Commit

---
+++ js/home.js
@@ -0,0 +1,231 @@
+/*
+//
+//var b='arfeen';
+//console.log(b);
+//var some_number=45;
+//console.log(some_number);
+//document.getElementById('sometext').innerHTML= 'age';
+
+//prompt('what is your age');
+//number in javascript
+//adding number in javascript
+var num1=24;
+num1--;
+//num1++;
+console.log(num1);
+//dividin the number
+console.log(num1 % 6);
+//inceremnt decrem what ever you want
+num1 +=10;
+console.log(num1);
+//function in javascript
+
+function fun(){
+    console.log('this is a function');
+
+}
+//call
+fun();
+function gretting(yourName){
+  
+    var result='hello'+yourName;
+    console.log(result);
+}
+var name=  prompt('what is you name');
+gretting(name);
+
+function Sum(num1,num2)
+{
+    var result=num1 + num2;
+    console.log(result);
+
+
+}
+Sum(10,10);
+//while loops
+var num=0;
+while(num <100){
+    num+=1
+    console.log(num);
+}
+//forloop
+
+for (let num=0;num<=100;num++ )
+{
+    console.log(num);
+}
+//Data types
+/*
+let yourAge=18;
+let yourName="arfeen";
+let name={first:'jane',last:'doe'};// 
+let truth=false;//Boolean
+let groceries=['apple','banana','mango']//array
+let Random;
+let nothing=null;
+
+//Strings in javascript
+let fruit="banana";
+let morefruits='bananaan/apple';//new line functionality
+console.log(fruit.length);//length function
+console.log(fruit.slice(2,6));//slice funtion
+console.log(fruit.replace('nana','123'));//replace
+console.log(fruit.toLowerCase());
+console.log(fruit.toUpperCase());
+console.log(fruit.split(','));
+console.log(fruit.split(''));
+
+//arrays in javascript
+/*
+let fruitee=['aaple','baanana','mango'];
+alert(fruitee[1]);//acccess values at through indexing
+fruitee[0]='pear';
+console.log(fruitee);
+
+//how to iterate arraylist through loop
+for(let i= 0 ;i<fruitee.length;i++);
+{
+    console.log(fruitee[i]);
+}
+
+// Arrray Common methods
+console.log('to string',fruitee.tostring());
+console.log(fruitee.join)('*');
+console.log(fruits.pop(),fruits);
+console.log(fruits.push)('blackberry');
+fruits.shift();
+fruits.unshift();
+//Combine Arrays
+
+let fruitee=['aaple','baanana','mango'];
+let vegetables=['ccucu','ffhfhf','gggg']
+let allgroceries=fruitee.concat(vegetables);
+
+console.log(allgroceries.reverse());
+
+
+
+
+//sorting of numbers
+let numbers=[2,9,9,33,2];
+console.log(numbers.sort());
+//asending order and descending order sorting
+console.log(numbers.sort(function(a,b){return a-b}));
+console.log(numbers.sort(function(a,b){return b-a}));
+
+
+
+//empty array for loop
+let emptyarray= new Array();
+for (let num=0;num<=10;num++){
+    emptyarray.push(num);
+
+}
+console.log(emptyarray);
+
+//objects in javasxript
+student ={firstName:'raafy',lastName:'qazi',age:20,height:170,studentInfo: function(){ return this.firstName +'/n',this.lastName +'/n',this.age +'/n',this.height +'/n/'}};
+
+
+console.log(student.studentInfo());
+
+//if statements in javascript
+var age=prompt("What is your age");
+if((age>=20) && (age<=50)){
+    status ="target demo"
+}
+    else {
+
+        status="my audiance"
+    }
+    */
+// switch statements
+/*
+switch(3){
+    case 0:
+    text='weekand';
+  break;
+
+    case 1:
+    text='weekday';
+
+
+break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    case 2:
+    text='weekday';
+    break;
+}
+console.log(text);
+   
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/list.js
@@ -0,0 +1,20 @@
+function myList(){
+    var item=document.getElementById("input").value;
+    var text=document.createTextNode(item);
+    
+    var newItem=document.createElement("li");
+    newItem.appendChild(text);
+    document.getElementById("myList").appendChild(newItem);
+    console.log(newItem);
+    
+    
+    }
+    
+    function deleteList(){
+        var list=document.getElementById("myList");
+        list.removeChild(list.childNodes[0]);
+       
+
+
+    }
+    
